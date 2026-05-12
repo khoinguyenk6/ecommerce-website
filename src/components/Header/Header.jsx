ce6acd1a18cf609534dddacfbd46a2967c875015
khoinguyenk6 <khoinguyen110206@gmail.com>
2026-05-12 23:59:09 +0700

    logo chua hien thi duoc

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,28 @@ import BoxIcon from "./BoxIcon/BoxIcon";
 import Menu from "./Menu/Menu";
 import { dataBoxIcon, dataMenu } from "./constans";
 import styles from "./styles.module.scss";
+import Logo from "@icons/images/logo.jpg"
 
 function MyHeader() {
-    const { containerBoxIcon , containerMenu } = styles;
+    const {containerBox, containerBoxIcon , containerMenu ,containerHeader} = styles;
     return ( 
-    <div>
-        <div>
+    <div className={containerHeader}>
+        <div className={containerBox}>
             <div className={containerBoxIcon}>
                 {dataBoxIcon.map((item) =>{
                     return <BoxIcon type={item.type} href={item.href} />
                 })}
             </div>
             <div className={containerMenu}>
-                {dataMenu.map((item) =>{
+                {dataMenu.slice(0,3).map((item) =>{
                     return <Menu content={item.content} href={item.href} />
                 })}
             </div>
         </div>
-        <div></div>
-        <div></div>
+        <div>
+            <img src="@icons/images/logo.jpg" alt="" />
+        </div>
+        <div>khoi ben phai</div>
     </div> 
     );
 }
